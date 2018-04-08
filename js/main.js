41b9dd78fbc8514f85bf0512a46eb2412c495d89
damianld <damilores@gmail.com>
2018-04-08 22:26:00 +0200

    Cambios para facer funcinal o proxecto

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-
-angular.
-	module('almacenApp',['artApp']).
-	controller('mainCtrl', function($scope) {
-
-	}).
-	filter('currency', function() {
-		return function currency(texto) {
-			return  parseFloat(texto).formato();
-		}
-
-	}).
-
-	filter('vfecha', function() {
-		return function vfecha(texto) {
-			return (texto)?texto.fv():'';
-		}
-	});
+angular
+  .module('app', ['app.articulos'])
+  .controller('PrincipalController', function($scope) {})
+  .filter('currency', function() {
+    return function currency(texto) {
+      return parseFloat(texto).formato();
+    };
+  })
+  .filter('vfecha', function() {
+    return function vfecha(texto) {
+      return texto ? texto.fv() : '';
+    };
+  });
